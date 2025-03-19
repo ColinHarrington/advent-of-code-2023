bfe52ffcd18b6204ea4e9d7eea90da027fe015af
Colin Harrington <colin.harrington@gmail.com>
2025-03-18 23:54:15 -0500

    Day 15 part 1

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,22 +1,18 @@
-use std::ops::Mul;
-
 advent_of_code::solution!(15);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    Some(input.trim()
-        .split(',')
-        .map(|s|hash(s) as u32)
-        .sum())
+    Some(input.trim().split(',').map(|s| hash(s) as u32).sum())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(_input: &str) -> Option<u32> {
     None
 }
 
 fn hash(input: &str) -> u8 {
-    input.chars()
-        .map(|c|c as u8)
-        .fold(0u8, |acc,c|acc.overflowing_add(c).0.overflowing_mul(17).0)
+    input
+        .chars()
+        .map(|c| c as u8)
+        .fold(0u8, |acc, c| acc.overflowing_add(c).0.overflowing_mul(17).0)
 }
 #[cfg(test)]
 mod tests {
