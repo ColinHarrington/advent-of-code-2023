ade6f2d5ba8c2371cdb5fc4a8c275d51eaa3eaf9
Colin Harrington <colin.harrington@gmail.com>
2025-06-28 16:05:36 -0500

    Day 13: Part 2

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -3,6 +3,14 @@ use itertools::Itertools;
 advent_of_code::solution!(13);
 
 pub fn part_one(input: &str) -> Option<usize> {
+    solve(input, reflection)
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    solve(input, smudgy_reflection)
+}
+
+fn solve(input: &str, reflection_finder: fn(Vec<String>) -> Vec<usize>) -> Option<usize> {
     let patterns = input
         .split("\n\n")
         .map(|p| p.lines().map(|s| s.to_string()).collect_vec())
@@ -13,14 +21,14 @@ pub fn part_one(input: &str) -> Option<usize> {
         .map(rotate_pattern)
         .collect_vec();
 
-    let horizontal: usize = patterns.into_iter().flat_map(reflections).sum();
-    let vertical: usize = rotated_patterns.into_iter().flat_map(reflections).sum();
+    let horizontal: usize = patterns.into_iter().flat_map(reflection_finder).sum();
+    let vertical: usize = rotated_patterns
+        .into_iter()
+        .flat_map(reflection_finder)
+        .sum();
     Some(horizontal * 100 + vertical)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
-}
 fn rotate_pattern(pattern: Vec<String>) -> Vec<String> {
     let grid = pattern
         .iter()
@@ -30,15 +38,35 @@ fn rotate_pattern(pattern: Vec<String>) -> Vec<String> {
         .map(|column| grid.iter().map(|row| row[column]).join(""))
         .collect()
 }
-fn reflections(pattern: Vec<String>) -> Vec<usize> {
+fn reflection(pattern: Vec<String>) -> Vec<usize> {
     (1..pattern.len())
         .filter(|&i| {
             pattern[0..i]
                 .iter()
                 .rev()
                 .zip(pattern[i..].iter())
-                // .inspect(|(up, down)| println!("[{i}]:\n{up}\n{down}\n"))
                 .all(|(l, r)| l.eq(r))
+        })
+        .collect_vec()
+}
+fn smudgy_reflection(pattern: Vec<String>) -> Vec<usize> {
+    (1..pattern.len())
+        .filter(|&i| {
+            pattern[0..i]
+                .iter()
+                .rev()
+                .zip(pattern[i..].iter())
+                .flat_map(|(l, r)| {
+                    l.chars()
+                        .zip(r.chars())
+                        .map(|(a, b)| match a == b {
+                            true => 0,
+                            false => 1,
+                        })
+                        .collect_vec()
+                })
+                .sum::<usize>()
+                == 1
         })
         .collect_vec()
 }
@@ -56,6 +84,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(400));
     }
 }
