ebee543f379639a7acf2bccc004c33f42382f020
Colin Harrington <colin.harrington@gmail.com>
2025-07-04 00:08:41 -0500

    Day 14: Part2 It's slow, but it works. Way too much memory allocation, I'll look at cleaning it up later

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,6 +1,6 @@
 use itertools::Itertools;
 use std::cmp::Ordering;
-use std::fmt::{Display, Formatter};
+use std::collections::HashMap;
 
 advent_of_code::solution!(14);
 
@@ -14,141 +14,31 @@ pub fn part_one(input: &str) -> Option<usize> {
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    let mut platform = Platform {
-        data: input
-            .lines()
-            .map(|line| line.chars().collect_vec())
-            .collect_vec(),
-    };
-    println!("{platform}");
-    platform.cycle().cycle().cycle();
-    None
-}
-
-#[derive(Debug, Clone, Hash, PartialEq, Eq)]
-struct Platform {
-    data: Vec<Vec<char>>,
-}
-impl Platform {
-    fn from_input(input: &str) -> Self {
-        let data = input
-            .lines()
-            .map(|line| line.chars().collect_vec())
-            .collect_vec();
-        let size = data.len();
-        Platform {
-            data: (0..size)
-                .map(|row| (0..size).map(|col| data[col][size - 1 - row]).collect_vec())
-                .collect(),
+    let mut data = input
+        .lines()
+        .map(|line| line.chars().collect_vec())
+        .collect_vec();
+    // let mut c = 0usize
+    let mut cache: HashMap<Vec<Vec<char>>, usize> = HashMap::new();
+    let mut rounds: Vec<Vec<Vec<char>>> = vec![];
+    for c in 0..1000000000usize {
+        data = cycle(data.clone());
+        rounds.push(data.clone());
+        if let Some(existing) = cache.insert(data.clone(), c) {
+            let cycle_length = c - existing;
+            let index = (1000000000usize - existing) % cycle_length + (existing - 1);
+            println!("Cycle Length:{c}: {existing} :: {index}");
+            return Some(score(rounds[index].clone()));
         }
     }
-    fn score(&self) -> usize {
-        let size = self.data.len();
-        self.data
-            .iter()
-            .enumerate()
-            .flat_map(|(row, line)| {
-                line.iter()
-                    .enumerate()
-                    .filter_map(move |(col, ch)| match ch {
-                        'O' => Some(size - row),
-                        _ => None,
-                    })
-            })
-            .sum::<usize>()
-    }
 
-    /// Each cycle tilts the platform four times so that the rounded rocks roll north, then west, then south, then east.
-    fn cycle(&mut self) -> Platform {
-        self.tilt_north();
-        // println!("{self}");
-        self.tilt_west();
-        // println!("{self}");
-        self.tilt_south();
-        // println!("{self}");
-        self.tilt_east();
-        println!("{self}");
-        self.clone()
-    }
-    fn rotate_right(&mut self) {
-        let size = self.data.len();
-        self.data = (0..size)
-            .map(|row| {
-                (0..size)
-                    .map(|col| self.data[col][size - 1 - row])
-                    .collect_vec()
-            })
-            .collect();
-    }
-
-    fn tilt_north(&mut self) {
-        let size = self.data.len();
-
-        let columns = (0..size)
-            .map(|col| roll((0..size).map(|row| self.data[row][col]).collect_vec()))
-            .collect_vec();
-        columns.into_iter().enumerate().for_each(|(col, column)| {
-            column
-                .into_iter()
-                .enumerate()
-                .for_each(|(row, ch)| self.data[row][col] = ch)
-        })
-    }
-
-    fn tilt_east(&mut self) {
-        let size = self.data.len();
-        let columns = (0..size)
-            .map(|row| roll(self.data[row].clone().into_iter().rev().collect_vec()))
-            .collect_vec();
-        columns.into_iter().enumerate().for_each(|(row, column)| {
-            column
-                .into_iter()
-                .enumerate()
-                .for_each(|(col, ch)| self.data[row][size - 1 - col] = ch)
-        })
-    }
-
-    fn tilt_south(&mut self) {
-        let size = self.data.len();
-        let columns = (0..size)
-            .map(|col| {
-                roll(
-                    (0..size)
-                        .map(|row| self.data[size - 1 - row][col])
-                        .collect_vec(),
-                )
-            })
-            .collect_vec();
-        columns.into_iter().enumerate().for_each(|(col, column)| {
-            column
-                .into_iter()
-                .enumerate()
-                .for_each(|(row, ch)| self.data[size - 1 - row][col] = ch)
-        })
-    }
-    fn tilt_west(&mut self) {
-        let size = self.data.len();
-        let columns = (0..size)
-            .map(|row| roll(self.data[row].clone()))
-            .collect_vec();
-        columns.into_iter().enumerate().for_each(|(row, line)| {
-            line.into_iter()
-                .enumerate()
-                .for_each(|(col, ch)| self.data[row][col] = ch)
-        })
-    }
-}
-impl Display for Platform {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let lines: Vec<String> = self
-            .data
-            .iter()
-            .map(|row| row.iter().join(""))
-            .collect_vec();
-        writeln!(f, "{}\n", lines.join("\n"))
-    }
+    Some(1)
 }
 
+/// Each cycle tilts the platform four times so that the rounded rocks roll north, then west, then south, then east.
+pub fn cycle(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    tilt_east(tilt_south(tilt_west(tilt_north(data))))
+}
 fn tilt_north(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
     let size = data.len();
     let columns = (0..size)
@@ -159,30 +49,40 @@ fn tilt_north(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
         .map(|row| (0..size).map(|col| columns[col][row]).collect_vec())
         .collect()
 }
-
-fn rotate_right(platform: Platform) -> Platform {
-    let size = platform.data.len();
-    let data: Vec<Vec<char>> = (0..size)
-        .map(|row| {
-            roll(
-                (0..size)
-                    .map(|col| platform.data[col][size - 1 - row])
-                    .collect_vec(),
-            )
-        })
-        .collect();
-
-    Platform { data }
+fn tilt_west(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let size = data.len();
+    (0..size).map(|row| roll(data[row].clone())).collect_vec()
 }
-fn roll(line: Vec<char>) -> Vec<char> {
-    line.split_inclusive(|ch| *ch == '#')
-        .map(|group| {
-            group
+
+fn tilt_east(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let size = data.len();
+    (0..size)
+        .map(|row| {
+            roll(data[row].clone().into_iter().rev().collect_vec())
                 .into_iter()
-                .sorted_by(stone_sort)
-                .map(|x| *x)
+                .rev()
                 .collect_vec()
         })
+        .collect_vec()
+}
+fn tilt_south(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let size = data.len();
+    let columns = (0..size)
+        .map(|col| roll((0..size).map(|row| data[row][col]).rev().collect_vec()))
+        .collect_vec();
+
+    (0..size)
+        .map(|row| {
+            (0..size)
+                .map(|col| columns[col][size - 1 - row])
+                .collect_vec()
+        })
+        .collect()
+}
+
+fn roll(line: Vec<char>) -> Vec<char> {
+    line.split_inclusive(|ch| *ch == '#')
+        .map(|group| group.iter().sorted_by(stone_sort).copied().collect_vec())
         .concat()
 }
 
@@ -203,14 +103,17 @@ fn score(data: Vec<Vec<char>>) -> usize {
     data.iter()
         .enumerate()
         .flat_map(|(row, line)| {
-            line.iter()
-                .enumerate()
-                .filter_map(move |(col, ch)| match ch {
-                    'O' => Some(size - row),
-                    _ => None,
-                })
+            line.iter().filter_map(move |ch| match ch {
+                'O' => Some(size - row),
+                _ => None,
+            })
         })
         .sum::<usize>()
+}
+#[allow(dead_code)]
+fn print_platform(data: Vec<Vec<char>>) {
+    let lines: Vec<String> = data.iter().map(|row| row.iter().join("")).collect_vec();
+    println!("{}\n", lines.join("\n"))
 }
 
 // fn north_itr(size:usize) ->
@@ -228,5 +131,30 @@ mod tests {
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(64));
+    }
+    #[test]
+    fn test_cycle() {
+        let mut data = to_data(&advent_of_code::template::read_file("examples", DAY));
+        let cycles = [
+            &advent_of_code::template::read_example(&format!("{}-cycle-1", DAY)),
+            &advent_of_code::template::read_example(&format!("{}-cycle-2", DAY)),
+            &advent_of_code::template::read_example(&format!("{}-cycle-3", DAY)),
+        ];
+
+        data = cycle(data.clone());
+        assert_eq!(data, to_data(cycles[0]));
+
+        data = cycle(data.clone());
+        assert_eq!(data, to_data(cycles[1]));
+
+        data = cycle(data.clone());
+        assert_eq!(data, to_data(cycles[2]));
+    }
+
+    fn to_data(input: &str) -> Vec<Vec<char>> {
+        input
+            .lines()
+            .map(|line| line.chars().collect_vec())
+            .collect_vec()
     }
 }
