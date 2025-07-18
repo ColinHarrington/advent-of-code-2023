2992e4a1b1f400cf8978a41dbc7626d7a0d4e0e1
Colin Harrington <colin.harrington@gmail.com>
2025-07-18 15:40:33 -0500

    No test input for part 2

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -393,10 +393,4 @@ mod tests {
         let result = part_one(&advent_of_code::template::read_example("20-2"));
         assert_eq!(result, Some(11687500));
     }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
 }
